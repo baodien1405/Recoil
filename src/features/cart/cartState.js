c9e4ce8d3477b68a0cb8955e40401c39ed30a146
Dien Cap <diencap@kms.com.vn>
2021-02-03 10:57:23 +0700

    Initial commit

---
+++ src/features/cart/cartState.js
@@ -0,0 +1,40 @@
+import { atom, selector } from "recoil";
+
+export const cartState = atom({
+  key: "cart",
+  // each item in list has 3 keys: id, product and quantity
+  default: [],
+});
+
+export const cartTotal = selector({
+  key: "cartTotal",
+  get: ({ get }) => {
+    const cart = get(cartState);
+
+    return cart.reduce((total, item) => {
+      return total + item.product.price * item.quantity;
+    }, 0);
+  },
+});
+
+export const addToCart = (cart, product) => {
+  const newCart = [...cart];
+  const foundIndex = cart.findIndex((x) => x.id === product.id);
+
+  // Increase quantity if existing
+  if (foundIndex >= 0) {
+    newCart[foundIndex] = {
+      ...cart[foundIndex],
+      quantity: cart[foundIndex].quantity + 1,
+    };
+    return newCart;
+  }
+
+  // Add new item
+  newCart.push({
+    id: product.id,
+    product: product,
+    quantity: 1,
+  });
+  return newCart;
+};
